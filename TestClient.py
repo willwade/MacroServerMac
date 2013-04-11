0cde9645c67bb068dc96d975e2c2f753e7837de1
Will Wade <willwade@gmail.com>
2013-04-11 19:46:45 +0100

    updated to take all actions via the command line

--- TestClient.py
+++ TestClient.py
@@ -1,30 +1,44 @@
 import socket
 import argparse
+import logging
 
-data = """{e7ca10ca-4f8d-47ed-8902-178849c7a1d5}<command="mouse"\><subcommandid="change_location"\><value="100"\><direction="270"\><click="0"\><X_MEUser"@user1@"\><X_MELng"9"\><X_STAVersion="1.1.1.1261"\>
-"""
+def dataform(args):
+    data = '{'+args.pluginid+'}'
+    data += '<subject="'+args.subject+'"\>'
+    data += '<command ="'+args.command+'"\>'
+    for subcmd in args.subcommand.split('|'):
+        data += '<'+str(subcmd.split(':')[0])+'="'+subcmd.split(':')[1]+'"\>'
+    data += '<X_MEUser"@'+args.xmeuser+'@"\>'
+    data += '<X_MELng"'+str(args.xmelang)+'"\>'
+    data += '<X_STAVersion"'+args.xstaver+'"\>'
+    return data
 
 parser = argparse.ArgumentParser(prog='MindExpressTestClient',description='Pretends to be a MindExpress client')
-parser.add_argument('--host', type=str, default='localhost', help='Connect to what server? Default: localhost')
-parser.add_argument('--port', type=int, default=12000, help='Change the default Mind Express Port number. Default: 12000')
-parser.add_argument('--debug', action='store_true', help='Debug the server.')      
+# Basics
+parser.add_argument('--host','-ho', type=str, default='localhost', help='Connect to what server? Default: localhost')
+parser.add_argument('--port','-p', type=int, default=12000, help='Change the default Mind Express Port number. Default: 12000')
+parser.add_argument('--debug','-db', action='store_true', help='Debug the server, and recieve the debug notes')      
 parser.add_argument('--data', type=argparse.FileType('r'), help='Read data from a file')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0', help='Get version number')
+# All the components of a Server request
+parser.add_argument('--pluginid','-pid', type=str, default='e7ca10ca-4f8d-47ed-8902178849c7a1d5', help='The Plugin-ID of the server')
+parser.add_argument('--subject','-sbj', type=str, default='execute_and_get_modifiers', help='Send what subject')
+parser.add_argument('--command','-cmd', type=str, default='send_key', help='Send what command')
+parser.add_argument('--subcommand','-scmd', type=str, default='normalkey:h|modifier:0', help='Send what sub-command. Note: Needed subelements provided in a key:value pair')
+# All the MeX info
+parser.add_argument('--xmeuser','-xu', type=str, default='user1-4874-305132', help='MindExpress User')
+parser.add_argument('--xmelang','-xl', type=int, default='9', help='Language int')
+parser.add_argument('--xstaver','-xs', type=str, default='1.1.1.1261', help='Version ID of this client')
 args = parser.parse_args() 
+
+# Call like this
+# python TestClient.py -cmd mouse -scmd 'subcommandid:change_location|value:100|direction:0|click:0'
 
 # Connect to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((args.host, args.port))
-
 # Send the data
-if (args.data!=None):
-    data = args.data.read()
-
 len_sent = s.send(data)
-
-# Receive a response
-response = s.recv(len_sent)
-print 'Received: "%s"' % response
-
 # Clean up
 s.close()
+     
