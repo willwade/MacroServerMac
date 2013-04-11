d4b4943a8aeef827d84eb382863f3b11e8f8f26b
Will Wade <willwade@gmail.com>
2013-04-11 21:27:18 +0100

    updated path

--- MacroClient/ClientCmd.py
+++ MacroClient/ClientCmd.py
@@ -9,4 +9,4 @@ input = ''
 while input != 'END':
     input = raw_input('Enter command: ')
     if (input !='END'):
-        subprocess.Popen("python /Users/willwade/bin/MacroSeverMac/Client/Client.py "+input, shell=True)
+        subprocess.Popen("python /Users/willwade/bin/MacroSeverMac/MacroClient/Client.py "+input, shell=True)
